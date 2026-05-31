a3119cd98d8ea69948ecf8a51ad1501a24377a6c
The sims official <t4291959@gmail.com>
2026-05-31 12:43:56 +1200

    Delete src directory

--- src/resources/markdown/guidelines/pages.js
+++
@@ -1,7 +0,0 @@
-import GuidelinesUploading from "./uploading.md?raw";
-import GuidelinesModeration from "./moderation.md?raw";
-
-export default {
-    "uploading": GuidelinesUploading,
-    "moderation": GuidelinesModeration,
-};
